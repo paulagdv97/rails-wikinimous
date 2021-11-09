cd441d7fba4a3c22c6b17d6a72bec3292c704cb6
paula garcia <paula.garciadv@gmail.com>
2021-11-09 16:46:36 +0100

    wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+10.times do
+  Article.create(title:Faker::Coffee.blend_name,
+                 content:Faker::Quote.famous_last_words)
+end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
